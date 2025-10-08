6dba4010f6bc9e405cc2fd16996f3c79647a99e1
miyamo2 <miyamo2@outlook.com>
2025-10-09 00:22:37 +0900

    fix: keda helm

--- terraform/modules/k8s/keda/main.tf
+++ terraform/modules/k8s/keda/main.tf
@@ -1,7 +1,5 @@
 resource "helm_release" "this" {
   name             = "keda"
   chart            = "keda"
-  namespace        = "keda"
-  create_namespace = true
   repository       = "https://kedacore.github.io/charts"
 }
