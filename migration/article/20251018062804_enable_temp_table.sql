f49ce11061046eeda8c76fd0feea99a16c22ce54
miyamo2 <miyamo2@outlook.com>
2025-10-18 16:14:59 +0900

    Revert "fix(cockroachdb): enable temp table as cluster setting"
    This reverts commit 47d924a818ff35e99f5d247c049832067962f9b8.

--- migration/article/20251018062804_enable_temp_table.sql
+++ migration/article/20251018062804_enable_temp_table.sql
@@ -1,6 +1,6 @@
 -- +goose NO TRANSACTION
 -- +goose Up
-SET CLUSTER SETTING enable_experimental_alter_column_type_general = on;
+SET enable_experimental_alter_column_type_general = true;
 
 -- +goose Down
-SET CLUSTER SETTING enable_experimental_alter_column_type_general = on;
+SET enable_experimental_alter_column_type_general = true;
